52f0bf9aba6b040b536f2bcd8315713e72105356
Mark <GuoJiafeng@users.noreply.github.com>
2020-07-23 14:37:11 +0800

    Update SMSUtil.java
    1111

--- src/main/java/io/yg/util/SMSUtil.java
+++ src/main/java/io/yg/util/SMSUtil.java
@@ -22,8 +22,8 @@ public class SMSUtil {
         final String product = "Dysmsapi";//短信API产品名称（短信产品名固定，无需修改）
         final String domain = "dysmsapi.aliyuncs.com";//短信API产品域名（接口地址固定，无需修改）
 //替换成你的AK
-        final String accessKeyId = "LTAIlQEwcWBaqBZX";//你的accessKeyId,参考本文档步骤2
-        final String accessKeySecret = "pTPG0MRrzDvEmRVkrOAGIVFWXznVRC";//你的accessKeySecret，参考本文档步骤2
+        final String accessKeyId = "";//你的accessKeyId,参考本文档步骤2
+        final String accessKeySecret = "";//你的accessKeySecret，参考本文档步骤2
 //初始化ascClient,暂时不支持多region（请勿修改）
         IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId,
                 accessKeySecret);
@@ -36,7 +36,7 @@ public class SMSUtil {
         //必填:待发送手机号。支持以逗号分隔的形式进行批量调用，批量上限为1000个手机号码,批量调用相对于单条调用及时性稍有延迟,验证码类型的短信推荐使用单条调用的方式
         request.setPhoneNumbers(phone);
         //必填:短信签名-可在短信控制台中找到
-        request.setSignName("郭家丰");
+        request.setSignName("");
         //必填:短信模板-可在短信控制台中找到
         request.setTemplateCode("SMS_115395562");
         //可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
